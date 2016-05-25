88922b020780b21cf28f030a541fef70f87d2dba
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-05-25 17:39:13 -0500

    paloma

---
+++ prueba/prueba/Assets/Level1/Script/PalomaBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PalomaBehaviour : MonoBehaviour {
+
+	private Vector3 movementVector;
+	private bool flip;
+
+	// Use this for initialization
+	void Start () {
+		flip = false;
+		movementVector = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+
+		if (Input.GetKey ("left")) {
+			if (flip) {
+				transform.localEulerAngles -= new Vector3 (0, 180, 0);
+				flip = false;
+			}
+			transform.localPosition -= new Vector3 (0.05f, 0, 0);
+			(GetComponent<Animator> ()).SetBool ("isVolando", true);
+		} else if (Input.GetKey ("right")) {
+			if (!flip) {
+				transform.localEulerAngles = new Vector3 (0, 180, 0);
+				flip = true;
+			}
+
+			transform.localPosition += new Vector3 (0.05f, 0, 0);
+			(GetComponent<Animator> ()).SetBool ("isVolando", true);
+		} else if (Input.GetKey ("return")) {
+			(GetComponent<Animator> ()).SetBool ("isVolando", true);
+			transform.localPosition += new Vector3 (0, 0.09f, 0);	
+		}
+		else {
+	
+			(GetComponent<Animator>()).SetBool("isVolando",false);
+		}
+
+	}
+}
---
+++ prueba/prueba/Assets/PisoColision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PisoColision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col){ //cuando chocan la primera vez
+		Debug.Log("entro en colision");
+	}
+
+	void OnCollisionStay2D(Collision2D col){ // mientras estan en contacto
+		Debug.Log ("Esta en colision con:" + col.gameObject.name);
+	}
+
+	void OnCollisionExit2D(Collision2D col){ //cuando dejan de estar en contacto
+	
+	}
+}
